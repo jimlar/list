102938e99c2dd9dbfda8792cf7e1a77c5321bee5
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2011-11-20 16:09:45 +0100

    added all libs to checked in and added monger dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject list "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.0"]]
+                           [noir "1.2.0"]
+                           [com.novemberain/monger "0.11.0-SNAPSHOT"]]
             :main list.server)
 
