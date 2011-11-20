7f3b0d701e52ffcb7f3153640f56802c194243bf
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2011-11-20 18:53:46 +0100

    successful mongo query

--- src/list/models/lists.clj
+++ src/list/models/lists.clj
@@ -1,6 +1,8 @@
 (ns list.models.lists
   (:require [monger.collection :as collection]))
 
+(monger.core/connect!)
+(monger.core/set-db! (monger.core/get-db "list"))
 
 (defn all []
-  (collection/find "list-items"))
+  (collection/find "lists"))
