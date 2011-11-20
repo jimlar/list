a9023940d8eebb6e9b1e19e41f5059767fa91710
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2011-11-20 14:58:47 +0100

    initial commit

---
+++ src/list/server.clj
@@ -0,0 +1,11 @@
+(ns list.server
+  (:require [noir.server :as server]))
+
+(server/load-views "src/list/views/")
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (server/start port {:mode mode
+                        :ns 'list})))
+
---
+++ src/list/views/common.clj
@@ -0,0 +1,13 @@
+(ns list.views.common
+  (:use noir.core
+        hiccup.core
+        hiccup.page-helpers))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "list"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
