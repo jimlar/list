d2567f50eb2992d047a1bcceebf7ca704583b69b
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-01-22 10:51:51 +0100

    converted to congomongo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject list "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "A little todo list app"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.0"]
-                           [com.novemberain/monger "0.11.0-SNAPSHOT"]]
-            :main list.server)
+                           [congomongo "0.1.7"]]
+  :main list.server)
 
--- src/list/models/lists.clj
+++ src/list/models/lists.clj
@@ -1,37 +1,37 @@
 (ns list.models.lists
-  (:require [monger.collection :as collection])
-  (:import  [org.bson.types ObjectId]))
+  (:import  [org.bson.types ObjectId])
+  (:use somnium.congomongo))
 
-(monger.core/connect!)
-(monger.core/set-db! (monger.core/get-db "list"))
+(def list-db (make-connection "list"))
+(set-connection! list-db)
 
 (defn to-objectid [str] (ObjectId. str))
 
-(defn list-id [l] (get l "_id"))
-(defn list-name [l] (get l "name"))
-(defn list-description [l] (get l "description"))
-(defn list-items [l] (sort-by (fn [x] (get x "weight" 0)) (filter #(not (get % "deleted" false)) (get l "items"))))
+(defn list-id [l] (:_id l))
+(defn list-name [l] (:name l))
+(defn list-description [l] (:description l))
+(defn list-items [l] (sort-by (fn [x] (get x :weight 0)) (filter #(not (get % :deleted false)) (:items l))))
 
-(defn item-id [i] (get i "id"))
-(defn item-name [i] (get i "name"))
+(defn item-id [i] (:id i))
+(defn item-name [i] (:name i))
 
 (defn all-lists []
-  (collection/find "lists"))
+  (fetch :lists))
 
 (defn list-by-id [id]
-  (collection/find-by-id "lists" id))
+  (fetch-by-id :lists id))
 
 (defn add-list [name, description]
-  (collection/insert "lists" {:name name, :description description, :items []}))
+  (insert! :lists {:name name, :description description, :items []}))
 
 (defn add-item [id, name]
   (let [list (list-by-id id)]
-    (collection/update "lists" {:_id id} {"$push" {:items {:id (ObjectId.) :name name :weight (inc (count (list-items list)))}}})))
+    (update! :lists {:_id id} {"$push" {:items {:id (ObjectId.) :name name :weight (inc (count (list-items list)))}}})))
 
 (defn remove-item [listid, itemid]
-  (collection/update "lists" {:_id listid "items.id" itemid} {"$set" {"items.$.deleted" true}}))
+  (update! :lists {:_id listid "items.id" itemid} {"$set" {"items.$.deleted" true}}))
 
 (defn reorder-items [id, item-ids]
   (dotimes [index (count item-ids)]
-    (collection/update "lists" {:_id id "items.id" (item-ids index)} {"$set" {"items.$.weight" (inc index)}})))
+    (update! :lists {:_id id "items.id" (item-ids index)} {"$set" {"items.$.weight" (inc index)}})))
 
