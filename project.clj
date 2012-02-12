a1ddd72dc8768e38726db90cc4d428b98af408cd
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-02-12 09:20:24 +0100

    added oauth

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "A little todo list app"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
-                           [congomongo "0.1.7"]]
+                           [congomongo "0.1.7"]
+                           [clj-oauth "1.2.10"]]
   :main list.server)
 
