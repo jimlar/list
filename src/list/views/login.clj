845c160a67a69f775529b76def842db09ea46769
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-02-12 10:13:53 +0100

    private is private

--- src/list/views/login.clj
+++ src/list/views/login.clj
@@ -4,22 +4,21 @@
             [noir.session :as session])
   (:use noir.core))
 
-
-(defn get-user []
-  (session/get :username))
-
-(defn set-user! [username]
-  (session/put! :username username))
-
-(defn logged-in? []
-  (not (nil? (get-user))))
-
 (def consumer (oauth/make-consumer "Il97ggeg5XrBL8njn4WKNgw4nQyPXXzzyf7inJ3p"
                 "y3VEDAizTBJnHxG6bnFfHKh0LRURFjMrjFzTHK1D"
                 "https://vauth.valtech.se/oauth/request_token"
                 "https://vauth.valtech.se/oauth/access_token"
                 "https://vauth.valtech.se/oauth/authorize"
                 :hmac-sha1))
+
+(defn get-user []
+  (session/get :username))
+
+(defn- set-user! [username]
+  (session/put! :username username))
+
+(defn logged-in? []
+  (not (nil? (get-user))))
 
 (defn request-login []
   (let [request-token (oauth/request-token consumer "/")]
