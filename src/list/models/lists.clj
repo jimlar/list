9068786f4cedd5482a58467e50cff0e4e69cdff7
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-02-18 23:15:01 +0100

    author of lists and items now saved

--- src/list/models/lists.clj
+++ src/list/models/lists.clj
@@ -19,11 +19,13 @@
 (defn to-objectid [str] (ObjectId. str))
 
 (defn list-id [l] (:_id l))
+(defn list-author [l] (:author l))
 (defn list-name [l] (:name l))
 (defn list-description [l] (:description l))
 (defn list-items [l] (sort-by (fn [x] (get x :weight 0)) (filter #(not (get % :deleted false)) (:items l))))
 
 (defn item-id [i] (:id i))
+(defn item-author [i] (:author i))
 (defn item-name [i] (:name i))
 
 (defn all-lists []
@@ -37,13 +39,13 @@
 (defn add-list [user name, description]
   (assert (instance? User user))
   (with-mongo list-db
-    (insert! :lists {:name name, :description description, :items []})))
+    (insert! :lists {:author (:email user), :name name, :description description, :items []})))
 
 (defn add-item [user id, name]
   (assert (instance? User user))
   (with-mongo list-db
     (let [list (list-by-id id)]
-      (update! :lists {:_id id} {"$push" {:items {:id (ObjectId.) :name name :weight (inc (count (list-items list)))}}}))))
+      (update! :lists {:_id id} {"$push" {:items {:id (ObjectId.), :author (:email user), :name name, :weight (inc (count (list-items list)))}}}))))
 
 (defn remove-item [user listid, itemid]
   (assert (instance? User user))
